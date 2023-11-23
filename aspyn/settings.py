488166d8fdffbc6cc5aaa32e2f4888ecda878392
Travis Myers <nucleus@Nucleus-59.lan>
2023-11-23 00:52:34 -0500

    Home page design

--- aspyn/settings.py
+++ aspyn/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "mainapp" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
